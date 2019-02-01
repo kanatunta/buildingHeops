7f67cfaed5182784be61652f703b88e186ffb7e9
Petar Nenov <petar_nenov@abv.bg>
2019-02-01 15:06:55 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+//Matrix aproach
+//To run type in terminal
+//npm start
+
+const testValue =22; // set test value of n, number of bricks
+
+let bricks = testValue;
+
+const pyramid = [];
+pyramid.push([]);
+pyramid[0] = [];
+pyramid[0][0] = "0";
+
+let row = 0;
+let col = 0;
+
+//loop to n>1 , because we set pyramid[0][0]='0' alredy and use 1 brick
+while (bricks > 1) {
+  if (row !== 0) {
+    row--;
+    col++;
+  } else {
+    row = col + 1;
+    col = 0;
+  }
+  //console.log(row,col) //uncommet to trace index movement ...
+
+  //add row or col id not exist
+  if (!pyramid[row]) {
+    pyramid.push([]);
+  }
+  if (!pyramid[row][col]) {
+    pyramid[row].push([]);
+  }
+
+  //set cell
+  pyramid[row][col] = "0";
+
+  //decrease number of bricks
+  bricks--;
+}
+
+//console.table(pyramid);
+console.log(beautifyResults(pyramid));
+console.log(`Columns are ${pyramid[0].length}`);
+
+//human readable format
+function beautifyResults(arr) {
+  let result = "";
+
+  for (const row of arr) {
+    let temp = "";
+    for (const col of row) {
+      temp += col;
+    }
+    result = temp + "\n" + result;
+  }
+  return result;
+}
---
+++ readme.md
@@ -0,0 +1,30 @@
+# [Pyramid](https://judge.telerikacademy.com/problem/21pyramids)  
+
+### _Coki went to Egypt once. Since then he loves pyramids._
+### _As all of us know, Coki also loves numbers..._
+### _Sooooo, the expected result is that Coki wants to make pyramids of blocks with numbers!_
+### _You have N blocks. With these blocks you should form pyramids._
+
+### **Each pyramid is actually a right-angled, isosceles triangle with one cathetus on the surface (see example). Also, you can think of the triangle as a set of columns, where the leftmost is 1 and the rightmost is the highest. The Kth column has exactly K blocks on top one another.**
+
+### _Your task is to find the total number of full columns, given a total of N blocks_
+
+```
+Input
+Read from the standard input
+On the single line, find the number N
+Output
+Print to the standard output
+
+Print the number of full columns for N
+
+Constraints
+1 <= N <= 2^50^
+```
+
+
+### **to run**
+
+```
+npm start
+```
